f625afb31c7a6e67c6d6bbede1272b2e59bce1a4
Guest User <Guest@Mac110-Dimir.local>
2013-01-25 14:36:44 -0500

    Created Program 0 Project

---
+++ cs132_program_0_asessandoh/cs132_program_0_asessandoh/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cs132_program_0_asessandoh
+//
+//  Created by Guest User on 1/25/13.
+//  Copyright (c) 2013 CS132. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
