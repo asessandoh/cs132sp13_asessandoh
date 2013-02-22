71a7220f2c451242cb3694c8bb64a44fccd0a3f1
Alex.E <asessandoh@gmail.com>
2013-02-22 07:42:01 -0500

    step 2b: result method added

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -39,7 +39,7 @@
         [self resultkey:pressKey];
         
     }
-    else if (isclearallkey(pressKey))
+    else if (isclearallkey(pressKey)) 
     {
         [self clearallkey:(pressKey)];
         
