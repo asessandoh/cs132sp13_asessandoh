8a5c1bfca76f0fd4134de4cf08531100eb0c7b56
Alex.E <asessandoh@gmail.com>
2013-02-28 15:49:48 -0500

    P2a setup

---
+++ ImmutableFraction.h
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.h
+//  
+//
+//  Created by Alex.E on 2/28/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImmutableFraction : NSObject
+
+@end
---
+++ ImmutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.m
+//  
+//
+//  Created by Alex.E on 2/28/13.
+//
+//
+
+#import "ImmutableFraction.h"
+
+@implementation ImmutableFraction
+
+@end
