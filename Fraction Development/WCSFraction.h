0290bf3493b4dd4f8762ab7d715dc1c6c4ca4ec2
Alex.E <asessandoh@gmail.com>
2013-03-22 00:46:30 -0400

    WCSFraction Class created

---
+++ Fraction Development/WCSFraction.h
@@ -0,0 +1,13 @@
+//
+//  WCSFraction.h
+//  Fraction Development
+//
+//  Created by Alex.E on 3/22/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCSFraction : NSObject
+
+@end
