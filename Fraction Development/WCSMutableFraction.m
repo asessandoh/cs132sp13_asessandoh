e0762e9ce28e224b310735971402eeb5170a9aff
Alex.E <asessandoh@gmail.com>
2013-03-26 15:18:08 -0400

    WCSMutableFraction class has been created

---
+++ Fraction Development/WCSMutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  WCSMutableFraction.m
+//  Fraction Development
+//
+//  Created by Alex.E on 3/26/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import "WCSMutableFraction.h"
+
+@implementation WCSMutableFraction
+
+@end
