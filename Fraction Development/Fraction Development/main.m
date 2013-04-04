6b963192449b79b4aba1bb48282be9f781bd458b
Alex.E <asessandoh@gmail.com>
2013-04-04 15:36:02 -0400

    Second task for prog2.e done

--- Fraction Development/Fraction Development/main.m
+++ Fraction Development/Fraction Development/main.m
@@ -17,8 +17,11 @@ int main(int argc, char *argv[])
     WCSFraction* oneThird ;
     WCSFraction* oneHalf ;
     WCSFraction* oneEight ;
-    
+    WCSFraction* fourhalves;
     WCSFraction* Result ;
+    WCSMutableFraction* Theans;
+    WCSMutableFraction* Ans;
+     WCSMutableFraction* mutant;
     
    oneThird = [oneThird initWithNumerator:1 andDenominator:3];
     oneHalf = [ oneHalf initWithNumerator:1 andDenominator:2];
@@ -27,8 +30,18 @@ int main(int argc, char *argv[])
     
     NSLog(@"the result is %d over %d",[Result numerator], [Result denominator]);
     
+    oneEight = [oneEight initWithNumerator:1 andDenominator:8];
     
-
+   
+    
+    [mutant setNumerator:5 andDenominator:8 ];
+    
+    Theans = [mutant modifyByNegative: oneEight];
+    Ans = [ mutant modifyByAdding: fourhalves];
+    
+    NSLog(@"theans is %d over %d",[Theans numerator], [Theans denominator]);
+    
+    NSLog(@"Ans is %d over %d",[Ans numerator], [Ans denominator]);
     
 
     
